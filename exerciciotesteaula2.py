548ce629389c94efa7b48c4d5d33138065ffca8d
LUCAS <funalo@exemplo.com>
2025-08-21 21:00:12 -0300

    testes aula 2

--- exerciciotesteaula2.py
+++ exerciciotesteaula2.py
@@ -2,6 +2,24 @@
 atributos de instância, métodos de classe e métodos de instância. 
 Os atributos devem ser privados e acessados somente via métodos públicos getters e setters'''
 
+class ContaBancaria:
+    def __init__(self, titular, saldo, senha):
+
+        
+        '''self.titular = titular          # Atributo público
+        self.__saldo = saldo            # Atributo privado
+        self.__senha = senha            # Atributo privado
+
+    def getSaldo(self):                # Método getter para saldo
+        return self.__saldo
+
+    def getSenha(self):                # Método getter para senha
+        return self.__senha
+
+    def setSenha(self, nova_senha):    # Método setter para senha
+        self.__senha = nova_senha'''
+
+
 conta = ContaBancaria("Ana", 1000, 123)
 
 print(conta.titular)         # Acesso direto (ok)
